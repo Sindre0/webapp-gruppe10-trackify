81f079d828ef807903db5a17e0b11c2c89a93d22
GalaxMax <galaxmax004@gmail.com>
2025-10-16 16:40:00 +0200

    Working towards routing logic

--- Trackify/src/app/components/Breadcrumbs.tsx
+++ Trackify/src/app/components/Breadcrumbs.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-dom";
 
 export default function Breadcrumbs(){
+
+    const getPathName = () => {
+        console.log(window.location.pathname);
+    } 
+
+    
+    
     return (
-        <nav style={{background: "lightgrey", width: "100%"}}>
+        <nav style={{background: "lightgrey", width: "100%", borderBottom: "2px solid black", borderTop: "2px solid black", fontWeight: "600"}}>
             <ul style={{padding: "0.2rem", paddingLeft: "0.5rem"}}>
-                <li>Home</li>
+                <li style={{display: "inline"}}><a href="/">Home</a>/</li>
+                <li style={{display: "inline"}}>Leaderboard</li>
             </ul>
         </nav>
     )
