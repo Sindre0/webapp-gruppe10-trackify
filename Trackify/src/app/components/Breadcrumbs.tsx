2526fc78c4d97c6441abe3efc23152f6b3a16867
GalaxMax <galaxmax004@gmail.com>
2025-10-17 12:48:47 +0200

    Leser request for å hente url

--- Trackify/src/app/components/Breadcrumbs.tsx
+++ Trackify/src/app/components/Breadcrumbs.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-dom";
+import { requestInfo } from "rwsdk/worker";
 
 export default function Breadcrumbs(){
+    const { request, response } = requestInfo;
 
-    const getPathName = () => {
-        console.log(window.location.pathname);
-    } 
-
-    
+    const getCurrentUrl = () => {
+        console.log(request?.url)
+        return <p>{request?.url}</p>
+    }
     
     return (
         <nav style={{background: "lightgrey", width: "100%", borderBottom: "2px solid black", borderTop: "2px solid black", fontWeight: "600"}}>
             <ul style={{padding: "0.2rem", paddingLeft: "0.5rem"}}>
                 <li style={{display: "inline"}}><a href="/">Home</a>/</li>
-                <li style={{display: "inline"}}>Leaderboard</li>
+                <li style={{display: "inline"}}><a href="/leaderboard">Leaderboard</a></li>
             </ul>
+            {getCurrentUrl()}
         </nav>
     )
 }
--- Trackify/src/worker.tsx
+++ Trackify/src/worker.tsx
@@ -22,33 +22,9 @@ export default defineApp([
   setCommonHeaders(),
   render(Document, [
     route("/", async () => {
-      const userResult = await drizzle(env.DB).select().from(users);
       return (  
         <div style={{ width: "100%", margin: "0 auto" }}>
           <Breadcrumbs/>
-          <h1>Start</h1>
-          <p>Velkommen til 2raws Branch!</p>
-          <p>Databasen har {userResult.length} brukere</p>
-          <div style={{ margin: "1.5rem 0" }}>
-            <a
-              href="/home"
-              style={{
-                display: "inline-block",
-                padding: "0.5rem 1rem",
-                background: "#0070f3",
-                color: "white",
-                textDecoration: "none",
-                borderRadius: "4px",
-                fontWeight: "500",
-              }}
-            >
-              Go to Home Page
-            </a>
-          </div>
-          <p style={{ fontSize: "0.875rem", color: "#666" }}>
-            Note: The home page is protected and requires authentication. You
-            will be redirected to login if you're not signed in.
-          </p>
         </div>
       );
     }),
@@ -63,5 +39,15 @@ export default defineApp([
       },
       Home,
     ]),
+    route("/leaderboard", [
+      async () => {
+      return (  
+        <div style={{ width: "100%", margin: "0 auto" }}>
+          <Breadcrumbs/>
+          This is a leaderboard site.
+        </div>
+      );  
+      },
+    ])
   ]),
 ]);
