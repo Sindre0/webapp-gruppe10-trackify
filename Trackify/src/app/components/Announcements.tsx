78b6a8352c638670d07f9dc075baeb7505602892
2rawwww <143406114+2rawwww@users.noreply.github.com>
2025-11-07 15:14:52 +0100

    Laget Announcements.tsx

--- Trackify/src/app/components/Announcements.tsx
+++ Trackify/src/app/components/Announcements.tsx
@@ -1,17 +1,22 @@
-export default function ConcludedPreview() {
-    const leaderboards = [
-        { id: 1, name: "Monopoly Game Night" },
-        { id: 2, name: "Marvel Rivals" },
-        { id: 3, name: "Sjakk HIOF sesong 3" },
+export default function Announcements() {
+    const announcements = [
+        { id: 1, name: "Brus og pizza" },
+        { id: 2, name: "Amogus i Aud 6.." },
+        { id: 3, name: "Lær C# med studentforeningen!" },
+        { id: 4, name: "FIFA Turnering" },
+        { id: 5, name: "Catan Championship" },
+        { id: 6, name: "Overwatch League" },
+        { id: 7, name: "Dungeons & Dragons Campaign" },
+
     ];
 
     return (
         <section className="block w-64">
-            <h2 className="m-2 text-lg">Concluded Leaderboards</h2>
+            <h2 className="m-2 text-lg">Announcements</h2>
             <ul className="px-5 py-3 border border-gray-300 shadow-md mx-auto mb-2">
-                {leaderboards.map((board) => (
-                    <li className="border-b border-black/20 mb-2 mt-2" key={board.id}>
-                        {board.name}
+                {announcements.map((element) => (
+                    <li className="border-b border-black/20 mb-2 mt-2" key={element.id}>
+                        {element.name}
                     </li>
                 ))}
                 <a className="block mt-4 text-blue-600 text-sm" href="#">View All </a>
